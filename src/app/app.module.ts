410953e969edb70a44e676d16634ef1d236483d9
Lee TB <tblyduc2412@gmail.com>
2023-03-18 16:51:16 +0700

    2. display list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   declarations: [AppComponent, HeroesComponent],
   providers: [],
   bootstrap: [AppComponent],
