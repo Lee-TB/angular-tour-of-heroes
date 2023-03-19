597ee0647490444c8e0c2c6974b984d4e603f548
Lee TB <tblyduc2412@gmail.com>
2023-03-19 14:32:59 +0700

    5. Add navigation - routing module, router outlet, navigation link,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, AppRoutingModule],
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
   providers: [],
   bootstrap: [AppComponent],
